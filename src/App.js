f81372944a6157bbdc7a211a3d787f5fe5014feb
rutIzhaki <ruti3250@gmail.com>
2021-12-26 15:11:52 +0200

    addAppBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './AppBar ';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
-    </div>
-  );
+
 }
 
 export default App;
---
+++ src/AppBar .js
@@ -0,0 +1,4 @@
+const AppBar = () => {
+    return <h1>Library</h1>
+}
+export default AppBar
