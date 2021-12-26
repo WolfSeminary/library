8f3e310b7936ec23de23f0317c94b620b2661797
hitmachut <hitmachut@students.local>
2021-12-26 14:09:31 +0200

    createBookCard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
+import Book from './Book';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
+      <Book book={{img:'./',name:'new book',info:'aaaa'}}/>
     </div>
   );
 }
