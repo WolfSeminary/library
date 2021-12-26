5ee3c5d42c087f9dcafb52b25febc590023d32d0
Rachely-Braverman <rachelb55677@gmail.com>
2021-12-26 14:55:52 +0200

    getListOfBooksFromGoogleBooks

--- src/books.js
+++ src/books.js
@@ -1,11 +1,10 @@
-export default books(() => {
+import { useEffect } from "react";
 
+export default books(() => {
 
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
             .then((res) => res.json())
     }, [])
-    
-
 
 });
