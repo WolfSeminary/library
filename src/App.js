beb48eefa149dc69cd89aaedad671030ed6493e5
Brachi-Waingarten <hitmachut@students.local>
2021-12-26 15:48:13 +0200

    type any message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NoBook from './NoBooks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
+   
     </div>
   );
 }
