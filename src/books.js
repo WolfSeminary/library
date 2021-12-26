0c914c388809c9585ccada1ef0499feee7f6fe57
Rachely-Braverman <rachelb55677@gmail.com>
2021-12-26 14:36:37 +0200

    getListOfBooksFromGoogleBooks

---
+++ src/books.js
@@ -0,0 +1,11 @@
+export default books(() => {
+
+
+    useEffect(() => {
+        fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
+            .then((res) => res.json())
+    }, [])
+    
+
+
+});
