d89e0abce79dcbaf7ce07d48de2f1a23ca51473a
zehava-drabkin <rdrbig@gmail.com>
2022-01-02 18:01:28 +0200

    set the atate of the books

--- src/books.js
+++ src/books.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default books(() => {
-
+const [books,setBooks]= useState()
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
-            .then((res) => res.json())
+            .then((res) => res.json(),
+            setBooks(res))
     }, [])
 
 });
