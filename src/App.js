0e33982f1737fceeed4aff2d1b0c916f059a277f
dasiHeizler <dasiheizler@gmail.com>
2022-01-02 16:15:00 +0200

    make the book clickable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
+import { Navigate, Route, useNavigate, Location, useLocation } from 'react-router-dom';
 import './App.css';
+import Book from './Book';
+import BookInfo from './BookInfo';
+
+import {
+  Routes
+} from "react-router-dom";
+
 
 function App() {
+  let location= useLocation();
+  let navigate = useNavigate();
+  const onBookClick = () => {
+    console.log("clicked");
+  // navigate("/BookInfo", { replace: true });
+  return <Navigate to="/BookInfo" state={{ from: location }} />
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+ <div onClick={onBookClick}>
+        <Book />
+      </div>
+      <Routes>
+        <Route path="/BookInfo" element={<BookInfo />} />
+        <Route path="/Book" element={<Book />} />
+      </Routes>
      
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
     </div>
+
   );
 }
 
---
+++ src/Book.js
@@ -0,0 +1,6 @@
+import { Route, Switch } from 'react-router-dom'
+export default function Book({Book}){
+    return (
+        <h1>book</h1>
+    )
+}
