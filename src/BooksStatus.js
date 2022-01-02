e20d5f4b184f249c09b30abc623585f1b14051ca
zehava-drabkin <rdrbig@gmail.com>
2022-01-02 18:38:58 +0200

    feat(BooksStatus)Borrowing books - Manage state

--- src/BooksStatus.js
+++ src/BooksStatus.js
@@ -1,7 +1,8 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
-const BooksStatus=()=>{
-    
+const BooksStatus=(book)=>{
+    const [bookStatus,setBookStatus] = useState([{id:book.id,status:"free"}])
     return(
         <>
       <Button variant="outlined">Library Status</Button>
--- src/books.js
+++ src/books.js
@@ -1,11 +1,28 @@
-import { useEffect, useState } from "react";
+import Book from "./Book"
+import React, { useState } from 'react'
+// import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+const Books = ({books}) => {
 
-export default books(() => {
-const [books,setBooks]= useState()
-    useEffect(() => {
-        fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
-            .then((res) => res.json(),
-            setBooks(res))
-    }, [])
-
-});
+    return (
+        <Box sx={{ justifyContent: "center" }}>
+          <Grid container justifyContent="center" spacing={{ xs: 1, md: 3 }} columns={{ xs: 2, sm: 8, md: 10 }}>
+            {books.map((book, index) => (
+              <Grid item xs={2} sm={2.5} md={4} key={index}>
+                <Item><Book book={book}/></Item>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      );
+}
+export default Books
