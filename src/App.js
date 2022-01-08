4bb73023059c38f626ace22c81cad30ed581a3e3
Eini Rachel Chaya <58681300+eini-rc@users.noreply.github.com>
2022-01-08 22:24:28 +0200

    feat(App): Add welcome header (#49)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
+        <h1>Welcome to library</h1>
       </header>
     </div>
   );
