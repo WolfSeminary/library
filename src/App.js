5e7b81ee910fcda5183a225afb59d771b5c88407
hitmachut <hitmachut@students.local>
2022-01-09 13:29:27 +0200

    addSwitchToBookComponent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import book from './Book';
+import Bookone from './Book';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-     
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
-        </p>
-      </header>
+  <Bookone/>
     </div>
   );
 }
