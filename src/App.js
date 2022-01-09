5f4281c0aa0ac8501a2232b1e58f299fae1a8613
rutIzhaki <ruti3250@gmail.com>
2022-01-09 13:23:55 +0200

    fixAppBarLibrary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
-import AppBar from './AppBar ';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to library</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/AppBar .js
+++ src/AppBar .js
@@ -1,4 +1,19 @@
-const AppBar = () => {
-    return <h1>Library</h1>
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function AppBarLibrary() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Library
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
-export default AppBar
