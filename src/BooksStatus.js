cc3a8701a664ed44e1aa0e326ed1f78d23fc14ec
hadasa-huminer <h0504112782@gmail.com>
2022-01-11 23:37:44 +0200

    organuze my page

--- src/BooksStatus.js
+++ src/BooksStatus.js
@@ -1,10 +1,10 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
-const BooksStatus=()=>{  
-    return(
-        <>
+const BooksStatus = () => {
+  return (
+    <>
       <Button variant="outlined">Library Status</Button>
-        </>
-    )   
-}
+    </>
+  );
+};
 export default BooksStatus;
