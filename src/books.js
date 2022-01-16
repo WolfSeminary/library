a7e2ce3e54cd7480ca5f63f96a52112a66e2d9a4
Rachely-Braverman <rachelb55677@gmail.com>
2022-01-16 14:21:46 +0200

    feat(Books): function defination

--- src/books.js
+++ src/books.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BooksStatus from './BooksStatus'
-export default Books(() => {
+
+export default Books = () => {
+    const [bokks, setBooks] = useState([])
+
+    const getBooksStatus = () => {
+    }
+
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
             .then((res) => res.json()).then(setBooks(res))
     }, [])
-    const getBooksStatus = () => {
-    }
+
     return (
         <>
             <div onClick={getBooksStatus}>
@@ -14,4 +19,4 @@ export default Books(() => {
             </div>
         </>
     )
-});
+}
