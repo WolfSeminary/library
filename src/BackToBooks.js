9eeec56cc3f431f8886955466ba4fe712ad10921
rutIzhaki <ruti3250@gmail.com>
2022-01-16 16:58:38 +0200

    AddNavigateToButton

--- src/BackToBooks.js
+++ src/BackToBooks.js
@@ -2,11 +2,14 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Stack from '@mui/material/Stack';
+import { useNavigate } from "react-router-dom";
 
 export default function BackToBooks() {
+  let navigate = useNavigate();
+  
   return (
     <Stack direction="row" spacing={2}>
-      <Button variant="outlined" startIcon={<ArrowBackIcon />}>
+      <Button variant="outlined" onClick={()=>navigate(-1)} startIcon={<ArrowBackIcon />}>
       Back to Books
       </Button>
     </Stack>
--- src/BookInfo.js
+++ src/BookInfo.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import BackToBooks from './BackToBooks';
 
 export default function BookInfo({book}) {
   return (
