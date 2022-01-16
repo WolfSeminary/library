d61f9a34175c0ae7fab60444d455c7d51097ad2a
zehava-drabkin <rdrbig@gmail.com>
2022-01-16 12:23:41 +0200

    feat(Books) set state of the books

--- src/books.js
+++ src/books.js
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 
 export default Books(() => {
-
+    const [books,setBooks] = setState([]);
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
-            .then((res) => res.json())
+            .then((res) => res.json()).then(setBooks(res))
     }, [])
 
 });
