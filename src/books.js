00ddcb124ac8223f9dc6e382ca62ba72d952af46
rutIzhaki <ruti3250@gmail.com>
2022-01-16 12:40:59 +0200

    showNoBookComponent

--- src/books.js
+++ src/books.js
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import NoBook from "./NoBooks";
 
 export default Books(() => {
-
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
-            .then((res) => res.json())
+            .then((res) => res.json)
     }, [])
-
+    return (
+        <>
+            {book.length == 0 && <NoBook />}
+        </>)
 });
