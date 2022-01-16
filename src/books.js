95a383ea66e82b8e571a869963e11c93b2424413
Hadasa Heizler <93077026+dasiHeizler@users.noreply.github.com>
2022-01-16 14:03:21 +0200

    feat(books)librarystatusevent (#57)

--- src/books.js
+++ src/books.js
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
-
+import BooksStatus from './BooksStatus'
 export default Books(() => {
-    const [books,setBooks] = setState([]);
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
             .then((res) => res.json()).then(setBooks(res))
     }, [])
-
+    const getBooksStatus = () => {
+    }
+    return (
+        <>
+            <div onClick={getBooksStatus}>
+                <BooksStatus />
+            </div>
+        </>
+    )
 });
