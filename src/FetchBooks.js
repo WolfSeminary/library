2d84df4aa225ff055a2c5af12e3c6ad83c143e9f
hadasa-huminer <h0504112782@gmail.com>
2022-01-16 12:42:12 +0200

    add onClick event

--- src/FetchBooks.js
+++ src/FetchBooks.js
@@ -2,9 +2,10 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 function FetchBooks() {
+    fetchDifferentBooks = () => { }
     return (
         <>
-            <Button variant="outlined">Fetch Books</Button>
+            <Button variant="outlined" onClick={fetchDifferentBooks}>Fetch Books</Button>
             <TextField id="outlined-basic" label="enter subject" variant="outlined" />
         </>
     )
