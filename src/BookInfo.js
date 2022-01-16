3f2840d329983a3667b0a3838da2e63af0936ffc
Chani-Heinrich <c0556723551@gmail.com>
2022-01-16 16:08:46 +0200

    addAButtonBack

--- src/BookInfo.js
+++ src/BookInfo.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 export default function BookInfo({book}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
+      <BackToBooks/>
         <Typography>
         Book Info
         </Typography>
