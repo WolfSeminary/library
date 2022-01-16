a748e4174dc237ae7abb14ad86836daf5b5b69b0
zehava-drabkin <rdrbig@gmail.com>
2022-01-16 13:31:21 +0200

    feat(BooksStatus) set the state

--- src/BooksStatus.js
+++ src/BooksStatus.js
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button";
 
-const BooksStatus = () => {
+const BooksStatus = (book) => {
+  const [bookStatus,setBookStatus] = useState([{id:book.id,status:"free"}])
   return (
     <>
       <Button variant="outlined">Library Status</Button>
