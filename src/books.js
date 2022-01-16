301ee33220cf23bf690fc73f7986ad8f6524c5f2
Rachely-Braverman <rachelb55677@gmail.com>
2022-01-16 12:25:49 +0200

    getListBooksFinito

--- src/books.js
+++ src/books.js
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 
-export default Books(() => {
+export default Books=()=>{
 
     useEffect(() => {
         fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
             .then((res) => res.json())
     }, [])
 
-});
+};
