aa60759bd98d9dcc6afe88bd8597d75e2763dc73
hadasa-huminer <h0504112782@gmail.com>
2022-01-19 18:28:54 +0200

    feat(books):renderBooks

--- src/books.js
+++ src/books.js
@@ -1,22 +1,52 @@
-import { useEffect, useState } from "react";
-import BooksStatus from './BooksStatus'
+import React, { useEffect, useState } from "react";
+import BooksStatus from "./BooksStatus";
+import BookInfo from "./BookInfo";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default Books = () => {
-    const [bokks, setBooks] = useState([])
+  const [bokks, setBooks] = useState([]);
 
-    const getBooksStatus = () => {
-    }
+  const getBooksStatus = () => {};
 
-    useEffect(() => {
-        fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
-            .then((res) => res.json()).then(setBooks(res))
-    }, [])
-
-    return (
-        <>
-            <div onClick={getBooksStatus}>
-                <BooksStatus />
-            </div>
-        </>
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40"
     )
-}
+      .then((res) => res.json())
+      .then(setBooks(res));
+  }, []);
+
+  return (
+    <>
+      <div onClick={getBooksStatus}>
+        <BooksStatus />
+      </div>
+      <Box sx={{ justifyContent: "center" }}>
+        <Grid
+          container
+          justifyContent="center"
+          spacing={{ xs: 1, md: 3 }}
+          columns={{ xs: 2, sm: 8, md: 10 }}
+        >
+          {books.map((book, index) => (
+            <Grid item xs={2} sm={2.5} md={4} key={index}>
+              <Item>
+                <BookInfo book={book} />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
+};
