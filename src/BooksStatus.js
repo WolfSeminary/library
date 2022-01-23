129e90e6f9bbc2f0d5e3cf4f007077d988f7389a
YaeliHaze <96684579+YaeliHaze@users.noreply.github.com>
2022-01-23 13:51:09 +0200

    Update src/BooksStatus.js
    Co-authored-by: Eini Rachel Chaya <58681300+eini-rc@users.noreply.github.com>

--- src/BooksStatus.js
+++ src/BooksStatus.js
@@ -1,7 +1,7 @@
 import Button from "@mui/material/Button";
 
 const BooksStatus = (book) => {
-  const [bookStatus,setBookStatus] = useState([{id:book.id,status:"free"}])
+  const [bookStatus,setBookStatus] = useState({id:book.id,status:"free"})
   return (
     <>
       <Button variant="outlined">Library Status</Button>
